ee7d3289e1a6b180eefba089c3952a5d5fe17092
FTQuin <quin.j.adam@gmail.com>
2022-03-22 13:55:20 -0700

    changed method names in feature_extractors_config.py

--- new_experiment_runner/feature_extractors_config.py
+++ new_experiment_runner/feature_extractors_config.py
@@ -6,16 +6,16 @@ class ExtractorAbstract:
     def __init__(self, num_features):
         self.num_features = num_features
     @abstractmethod
-    def pre_process_features(self, frames):
+    def pre_process_extract(self, frames):
         pass
 
     @abstractmethod
-    def post_process_features(self, features):
+    def train_extract(self, features):
         pass
 
-    def extract(self, frames):
-        raw_features = self.pre_process_features(frames)
-        processed_features = self.post_process_features(raw_features)
+    def live_extract(self, frames):
+        raw_features = self.pre_process_extract(frames)
+        processed_features = self.train_extract(raw_features)
         return processed_features
 
 
@@ -25,21 +25,22 @@ class MovenetExtractor(ExtractorAbstract):
         super(MovenetExtractor, self).__init__(num_features)
 
         # get movenet
-        self.model = tf.saved_model.load('../movenet/movenet_multipose_lightning_1') # keep ref to model
+        self.model = tf.saved_model.load('../movenet/movenet_multipose_lightning_1')  # keep ref to model
         self.movenet = self.model.signatures['serving_default']
 
         # threshold when outputting features
         self.threshold = threshold
 
     @tf.function(input_signature=(tf.TensorSpec(shape=[1, None, None, 3], dtype='int32'),))
-    def pre_process_features(self, frames):
+    def pre_process_extract(self, frames):
         t1 = tf.image.resize_with_pad(frames, 256, 256)  # resize and pad
         t2 = tf.cast(t1, dtype=tf.int32)  # cast to int32
         out = self.movenet(t2)['output_0']  # get result
         return out
 
     @tf.function(input_signature=(tf.TensorSpec(shape=[1, 6, 56], dtype='float32'),))
-    def post_process_features(self, features):
+    def train_extract(self, features):
+        # zero out vals below threshold
         cond = tf.less(features, tf.constant(self.threshold, dtype=features.dtype))
         out = tf.where(cond, tf.zeros(tf.shape(features), dtype=features.dtype), features)
         return tf.reshape(out, (1, -1))  # flatten
@@ -49,5 +50,5 @@ if __name__ == '__main__':
     img = tf.io.decode_jpeg(tf.io.read_file('../TFlite-NN/images/image.jpeg'))
     img = tf.expand_dims(img, axis=0)
     movenet = MovenetExtractor()
-    res = movenet.pre_process_features(img)
+    res = movenet.pre_process_extract(img)
     print(res.shape)
