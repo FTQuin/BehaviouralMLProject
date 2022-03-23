df431678a3d3c93be79ac94020b562f37aac1cd1
Govind <govind_tanda1@hotmail.com>
2022-03-22 18:04:15 -0700

    setup feature_extractors_config.py with inceptionv3 and mobilenetv2, added lstm models to models_config.py

--- new_experiment_runner/feature_extractors_config.py
+++ new_experiment_runner/feature_extractors_config.py
@@ -3,8 +3,10 @@ from abc import abstractmethod
 
 
 class ExtractorAbstract:
-    def __init__(self, num_features):
+    def __init__(self, num_features, name):
         self.num_features = num_features
+        self.name = name
+
     @abstractmethod
     def pre_process_extract(self, frames):
         pass
@@ -21,8 +23,9 @@ class ExtractorAbstract:
 
 class MovenetExtractor(ExtractorAbstract):
     def __init__(self, threshold=0.0):
-        num_features = 6*56
-        super(MovenetExtractor, self).__init__(num_features)
+        num_features = 6 * 56
+        name = "MovenetExtractor"
+        super(MovenetExtractor, self).__init__(num_features, name)
 
         # get movenet
         self.model = tf.saved_model.load('../movenet/movenet_multipose_lightning_1')  # keep ref to model
@@ -44,6 +47,58 @@ class MovenetExtractor(ExtractorAbstract):
         cond = tf.less(features, tf.constant(self.threshold, dtype=features.dtype))
         out = tf.where(cond, tf.zeros(tf.shape(features), dtype=features.dtype), features)
         return tf.reshape(out, (1, -1))  # flatten
+
+
+class InceptionExtractor(ExtractorAbstract):
+    def __init__(self, img_size):
+        num_features = 2048
+        name = "InceptionExtractor"
+        super(InceptionExtractor, self).__init__(num_features, name)
+        feature_extractor = tf.keras.applications.InceptionV3(
+            weights="imagenet",
+            include_top=False,
+            pooling="avg",
+            input_shape=(img_size, img_size, 3),
+        )
+        preprocess_input = tf.keras.applications.inception_v3.preprocess_input
+
+        inputs = tf.keras.Input((img_size, img_size, 3))
+        preprocessed = preprocess_input(inputs)
+
+        outputs = feature_extractor(preprocessed)
+        self.model = tf.keras.Model(inputs, outputs)
+
+    def pre_process_extract(self, frames):
+        return self.model(frames)
+
+    def train_extract(self, features):
+        return features
+
+
+class MobileNetV2Extractor(ExtractorAbstract):
+    def __init__(self, img_size):
+        num_features = 1280
+        name = "MobileNetV2Extractor"
+        super(MobileNetV2Extractor, self).__init__(num_features, name)
+        feature_extractor = tf.keras.applications.MobileNetV2(
+            weights="imagenet",
+            include_top=False,
+            pooling="avg",
+            input_shape=(img_size, img_size, 3),
+        )
+        preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+
+        inputs = tf.keras.Input((img_size, img_size, 3))
+        preprocessed = preprocess_input(inputs)
+
+        outputs = feature_extractor(preprocessed)
+        self.model = tf.keras.Model(inputs, outputs)
+
+    def pre_process_extract(self, frames):
+        return self.model(frames)
+
+    def train_extract(self, features):
+        return features
 
 
 if __name__ == '__main__':
--- new_experiment_runner/models_config.py
+++ new_experiment_runner/models_config.py
@@ -1,13 +1,10 @@
 from tensorflow import keras
 
 
-class model_abstract():
-    pass
-
-
-class GRU(model_abstract):
+class GRU:
     @staticmethod
-    def gru1(input_shape, output_size, seq_len=-1, activation_function='relu', loss_function="sparse_categorical_crossentropy", optimizer="adam"):
+    def gru1(input_shape, output_size, seq_len=-1, activation_function='relu',
+             loss_function="sparse_categorical_crossentropy", optimizer="adam"):
         frame_features_input = keras.Input((seq_len, input_shape))
         x = keras.layers.GRU(16, return_sequences=True)(
             frame_features_input
@@ -25,7 +22,8 @@ class GRU(model_abstract):
         return gru_model
 
     @staticmethod
-    def gru2(input_shape, output_size, seq_len=-1, activation_function='relu', loss_function="sparse_categorical_crossentropy", optimizer="adam"):
+    def gru2(input_shape, output_size, seq_len=-1, activation_function='relu',
+             loss_function="sparse_categorical_crossentropy", optimizer="adam"):
         model = keras.Sequential(
             [
                 keras.layers.GRU(160, return_sequences=True),
@@ -41,3 +39,40 @@ class GRU(model_abstract):
         model.compile(loss=loss_function, optimizer=optimizer, metrics=["accuracy"])
         return model
 
+
+class LSTM:
+    @staticmethod
+    def lstm1(input_shape, output_size, seq_len=-1, activation_function='relu',
+              loss_function="sparse_categorical_crossentropy", optimizer="adam"):
+        frame_features_input = keras.Input((seq_len, input_shape))
+        x = keras.layers.LSTM(16, return_sequences=True)(frame_features_input)
+        x = keras.layers.LSTM(8)(x)
+        x = keras.layers.Dropout(0.4)(x)
+        x = keras.layers.Dense(16, activation=activation_function)(x)
+        x = keras.layers.Dense(8, activation=activation_function)(x)
+        output = keras.layers.Dense(output_size, activation='softmax')(x)
+
+        lstm_model = keras.Model([frame_features_input], output)
+
+        lstm_model.compile(loss=loss_function, optimizer=optimizer, metrics=["accuracy"])
+
+        return lstm_model
+
+    @staticmethod
+    def lstm2(input_shape, output_size, seq_len=-1, activation_function='relu',
+              loss_function="sparse_categorical_crossentropy", optimizer="adam"):
+        frame_features_input = (input_shape, seq_len)
+        model = keras.Sequential(
+            [
+                keras.layers.LSTM(256, input_shape=frame_features_input, return_sequences=True),
+                keras.layers.LSTM(128),
+                keras.layers.Dropout(0.4),
+                keras.layers.Dense(256, activation=activation_function),
+                keras.layers.Dense(128, activation=activation_function),
+                keras.layers.Dense(64, activation=activation_function),
+                keras.layers.Dense(output_size, activation='softmax'),
+            ]
+        )
+
+        model.compile(loss=loss_function, optimizer=optimizer, metrics=["accuracy"])
+        return model
