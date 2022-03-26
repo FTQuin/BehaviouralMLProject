1e5df6d2c7960b0d6393fd2b548619fe98f603cf
FTQuin <quin.j.adam@gmail.com>
2022-03-25 19:59:03 -0700

    refactored preprocess_data.py

--- new_experiment_runner/preprocess_data.py
+++ new_experiment_runner/preprocess_data.py
@@ -34,71 +34,28 @@ def prepare_all_videos():
     :rtype: return type
     """
 
-    # pass frame_features into model
-    frame_features = pd.DataFrame()
-
     # For each video, get features
     for idx, video_info in enumerate(dataset.data_iterator):
-        temp_frame_features = prepare_one_video_parallel(video_info)
+        video_frame_features = prepare_one_video(video_info)
 
         print(f'Extracted features from video {idx}')
+
+        # make dir for video
         try:
-            os.mkdir(os.path.join(dataset.features_save_path, temp_frame_features['label'][0]))
+            os.mkdir(os.path.join(dataset.features_save_path, video_frame_features['label'][0]))
         except:
             pass
-        finally:
-            temp_frame_features.to_csv(os.path.join(dataset.features_save_path, temp_frame_features['label'][0],
-                                                    temp_frame_features['video'][0].replace('.avi', '.csv')))
-            print(f'Saved to  {os.path.join(dataset.features_save_path, temp_frame_features["label"][0], temp_frame_features["video"][0].replace(".avi", ".csv"))}')
+
+        # save features
+        video_frame_features.to_csv(os.path.join(dataset.features_save_path, video_frame_features['label'][0],
+                                                video_frame_features['video'][0].replace('.avi', '.csv')))
+        print(f'Saved to  {os.path.join(dataset.features_save_path, video_frame_features["label"][0], video_frame_features["video"][0].replace(".avi", ".csv"))}')
 
 
-def save_data(extracted_frame_pd):
-    try:
-        os.mkdir(dataset.features_save_path)
-    except:
-        pass
-    finally:
-        extracted_frame_pd.to_csv(os.path.join(dataset.features_save_path, FILE_NAME + '.zip'),
-                                  index=False, compression=dict(method='zip', archive_name=FILE_NAME + '.csv'))
-
-
-def prepare_all_videos_parallel():
-    # TODO: make docstring
-    """
-    description ...
-
-    :parm p1: parameter description
-    :type p1: parameter type
-    :return: return description
-    :rtype: return type
-    """
-
-    # pass frame_features into model
-    frame_features = pd.DataFrame()
-
-    # initialize multiprocessing pool
-    pool = mp.pool.ThreadPool(1)
-    with tf.device('/GPU:0'):
-        videos_iterator = pool.imap_unordered(prepare_one_video_parallel, dataset.data_iterator, chunksize=1)
-
-        for idx, temp_frame_features in enumerate(videos_iterator):
-            print('finished video:', idx + 1)
-            frame_features = pd.concat((frame_features, temp_frame_features), copy=False)
-            # try:
-            #     os.mkdir(os.path.join(dataset.features_save_path, temp_frame_features['label'][0]))
-            # except:
-            #     pass
-            # finally:
-            #     temp_frame_features.to_csv(os.path.join(dataset.features_save_path, temp_frame_features['label'][0],
-            #                                             temp_frame_features['video'][0].replace('.avi', '.csv')))
-
-    return frame_features
-
-
-def prepare_one_video_parallel(video_info):
+def prepare_one_video(video_info):
     frames = video_info['frames']
 
-    res = prepare_one_batch_parallel(frames)
+    res = feature_extractor.pre_process_extract_video(frames)
     res = tf.reshape(res, (res.shape[0], -1))
     temp_frame_features = pd.DataFrame(data=res.numpy())
 
@@ -109,23 +66,10 @@ def prepare_one_video_parallel(video_info):
     return temp_frame_features
 
 
-@tf.function(input_signature=(tf.TensorSpec(shape=[None, None, None, 3], dtype='int32'),))
-def prepare_one_batch_parallel(frames):
-    return tf.map_fn(feature_extractor.pre_process_extract, tf.expand_dims(frames, axis=1),
-                     fn_output_signature=tf.TensorSpec((1, 6, 56)),
-                     parallel_iterations=5,
-                     swap_memory=True,
-                     )
-
-
 if __name__ == '__main__':
     print('Preparing Data')
     # prepare data
     features = prepare_all_videos()
-
-    print('Saving Data')
-    # save data
-    save_data(features)
 
     # TODO: make more verbose
     print('DONE')
